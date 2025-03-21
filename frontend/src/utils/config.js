e00d216a4d6da5aee5e6a660b4fda13f5bfdf06e
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-03-21 19:03:30 +0300

    editing the the backend url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://127.0.0.1:8000/api/v1"
-export const BASE_URL = "https://syria-tours-app-backend.onrender.com"
+export const BASE_URL = "https://syria-tours-app-backend.onrender.com/api/v1"
