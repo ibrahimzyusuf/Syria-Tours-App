d28bd23b9642d5debe5df3dcf2411e6a60e978fc
ibrahimzyusuf <ibramhashalyusuf880@gmail.com>
2025-09-27 22:02:50 +0300

    also about user image

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { Container, Row, Col, Form, FormGroup, Button } from 'reactstrap'
 import { Link , useNavigate } from 'react-router-dom'
 import '../styles/login.css'
 import loginImg from '/images/lo.webp'
-import userIcon from '/images/user.webp'
+import userIcon from '/images/user.png'
 import { AuthContext } from '../Context/AuthContext'
 import { BASE_URL } from './../utils/config'
 
